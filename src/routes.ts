1d8f2d555cf1c4fe881640d737c4175e4523fa9c
Amanda Gundes <amandagundes@outlook.com>
2021-05-08 21:13:45 -0300

    Add User Authentication

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,20 @@ import { Router, Request, Response } from "express";
 import { ActivityController } from "./controller/ActivityController";
 import { CourseUnitController } from "./controller/CourseUnitController";
 import { UserController } from "./controller/UserController";
+import { AuthenticateController } from "./controller/AuthenticateController";
+import authenticated from "./middlewares/authenticated";
 
 const userController = new UserController();
 const activityController = new ActivityController();
 const courseUnitController = new CourseUnitController();
+const authenticateController = new AuthenticateController();
 
 const routes = Router();
 
 routes.post("/user", userController.create);
-routes.post("/activity", activityController.create);
-routes.post("/course_unit", courseUnitController.create);
+routes.get("/user", authenticated, userController.show);
+routes.post("/auth", authenticateController.create);
+routes.post("/activity", authenticated, activityController.create);
+routes.post("/course_unit", authenticated, courseUnitController.create);
 
 export default routes;
